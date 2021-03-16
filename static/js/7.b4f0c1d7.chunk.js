90f14933c9186f204e4b7346455a8bd7b7ff4d30
Roman-Denysenko <roman.denisenko83@gmail.com>
2021-03-16 23:19:30 +0200

    Updates

---
+++ static/js/7.b4f0c1d7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-09-phonebook"]=this["webpackJsonpgoit-react-hw-09-phonebook"]||[]).push([[7],{104:function(e,t,n){"use strict";n.r(t),n.d(t,"default",(function(){return a}));n(0);var o=n(91),c=n.n(o),i=n(1),a=function(){return Object(i.jsx)("div",{className:c.a.container,children:Object(i.jsx)("h1",{className:c.a.title,children:"Welcome to Phone-book Page"})})}},91:function(e,t,n){e.exports={container:"Home_container__11Enr",title:"Home_title__1BJtj"}}}]);
+//# sourceMappingURL=7.b4f0c1d7.chunk.js.map
